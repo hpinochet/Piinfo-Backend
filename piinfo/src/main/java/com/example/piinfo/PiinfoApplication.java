fad6362a821ae4b8b6ab6e88c3b96935bb5605e8
Hernan Pinochet <hernan.pinochet@usach.cl>
2022-12-06 20:56:33 -0300

    Se inicializa SpringBoot

---
+++ piinfo/src/main/java/com/example/piinfo/PiinfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.piinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiinfoApplication.class, args);
+	}
+
+}
---
+++ piinfo/src/test/java/com/example/piinfo/PiinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
